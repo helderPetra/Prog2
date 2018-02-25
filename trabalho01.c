2fe51e475a87282c78c2441f43c6b9eeaa0d4cf0
helderPetra <hpetra73@gmail.com>
2018-02-25 12:31:47 -0300

    exibir frase via função!

---
+++ trabalho01.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//função
+ void printar(char frase)
+ 	{
+ 		  printf("\n Boa Noite! \n");
+	 }
+
+//progrma principal
+ int main()
+{ 
+  char frase;
+   printar(frase);
+ 
+system("pause>NULL");
+return 0;
+}
+
